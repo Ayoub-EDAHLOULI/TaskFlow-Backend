6108d497fc2bed1e4e0f877233ee72b8e7fe2506
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-12-31 11:26:24 +0100

    Create UserController > First the DbContext and the first crud operation get all users

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using backend.Data;
+using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,32 +9,8 @@ namespace backend.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
         public DbSet<TaskItem> TaskItems { get; set; }
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>().HasData(
-                new User
-                {
-                    Id = 1,
-                    Username = "admin",
-                    Password = "admin",
-                    Role = Role.Admin
-                },
-                new User
-                {
-                    Id = 2,
-                    Username = "user",
-                    Password = "user",
-                    Role = Role.User
-                }
-            );
-        }
-
     }
 }
