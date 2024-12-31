869dc0bf19d556074f6df5487f6703677804464f
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-12-31 10:48:16 +0100

    Add project files.

---
+++ Models/TaskItem.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+        public bool IsImportant { get; set; }
+        public DateTime DueDate { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
