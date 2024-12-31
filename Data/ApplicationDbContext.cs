e46bf3030ef151a0a8e36076f5aa3cfb4a7b40b6
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-12-31 11:20:51 +0100

    seed data to user tabe > create two users with different roles admin and default user

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,5 +15,26 @@ namespace backend.Data
         public DbSet<User> Users { get; set; }
         public DbSet<TaskItem> TaskItems { get; set; }
 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User
+                {
+                    Id = 1,
+                    Username = "admin",
+                    Password = "admin",
+                    Role = Role.Admin
+                },
+                new User
+                {
+                    Id = 2,
+                    Username = "user",
+                    Password = "user",
+                    Role = Role.User
+                }
+            );
+        }
+
     }
 }
