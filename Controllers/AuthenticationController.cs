ac93fb6d603e2f6e6de9f3bd033f4e845e2117a3
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2024-12-31 15:34:18 +0100

    Create authentication Register method

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using backend.Data;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.Scripting;
+
+
+namespace backend.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        // POST: api/register
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register([FromBody] User user)
+        {
+            try
+            {
+                // Check if the user already exists
+                var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == user.Username);
+                if (existingUser != null)
+                {
+                    return StatusCode(400, "User already exists");
+                }
+
+                // Hash password
+                user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return Ok("User registered successfully.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
